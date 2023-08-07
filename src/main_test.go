0d56a1c2663ebbaf7737f75e853f67ec3882788a
矢島 達朗 <yajima.t@b-style.net>
2023-08-07 10:38:37 +0900

    💫 「何をテストしたいか」がわかりやすくなるようにテストを修正

--- src/main_test.go
+++ src/main_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-func Test_123abc_should_be_matched(t *testing.T) {
-	targetStrings := []string{"123abc"}
+func Test_invalid_sql_should_be_matched(t *testing.T) {
+	targetStrings := []string{"SELECT * FROM test_table WHERE id = 123UNION ALL"}
 
 	result := findMatchedStrings(targetStrings)
 
-	expected := []string{"123abc"}
+	expected := []string{"SELECT * FROM test_table WHERE id = 123UNION ALL"}
 
 	if !reflect.DeepEqual(result, expected) {
 		t.Errorf("got %v want %v", result, expected)
